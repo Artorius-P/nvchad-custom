273ff31f76af180b03ffb9763564ca4d2a6a1cf2
Artorius <dwxd@foxmail.com>
2022-07-29 22:36:24 +0800

    format

--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -1,16 +1,15 @@
-
 return {
-  ["sbdchd/neoformat"] = {},
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end,
-  },
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-      after = "nvim-lspconfig",
-      config = function()
-         require "custom.plugins.null-ls"
-      end,
-  }
+	["sbdchd/neoformat"] = {},
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.lspconfig")
+		end,
+	},
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
 }
